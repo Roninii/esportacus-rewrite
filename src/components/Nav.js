abf197636e027e3031ee7376f79e0817ff69fc81
Ronnie Villarini <ronnie@ronini.dev>
2019-05-31 16:02:51 -0500

    add nav link styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,20 +1,51 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core";
 import { NavLink } from "react-router-dom";
+
+const nav = css`
+  list-style-type: none;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const navLink = css`
+  margin-right: 1rem;
+  text-decoration: none;
+  color: inherit;
+  font-weight: 700;
+  font-size: 1.2rem;
+
+  &:hover {
+    color: #673ab7;
+  }
+`;
+
+const active = {
+  color: `#673ab8`
+};
 
 const Nav = () => {
   return (
-    <ul>
+    <ul css={nav}>
       <li>
-        <NavLink to="/">Home</NavLink>
+        <NavLink css={navLink} activeStyle={active} exact to="/">
+          Home
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/teams">Teams</NavLink>
+        <NavLink css={navLink} activeStyle={active} to="/teams">
+          Teams
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/players">Players</NavLink>
+        <NavLink css={navLink} activeStyle={active} to="/players">
+          Players
+        </NavLink>
       </li>
       <li>
-        <NavLink to="/leagues">Leagues</NavLink>
+        <NavLink css={navLink} activeStyle={active} to="/leagues">
+          Leagues
+        </NavLink>
       </li>
     </ul>
   );
